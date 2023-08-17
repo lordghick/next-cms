753efd1469353101548737fdaf55246aadf3f7d3
lordghick <lordghick@gmail.com>
2023-08-17 16:40:37 -0400

    firts commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import { RiMenuLine } from "react-icons/ri";
+
+function Header() {
+  return (
+    <header className="bg-white py-4 px-4 z-50">
+      <div className="mx-auto container max-w-sm md:max-w-2xl lg:max-w-6xl flex items-center justify-between">
+        <div className="">
+          <Link href="/" className="text-blue-500 font-bold text-2xl">
+            Logo
+          </Link>
+        </div>
+
+        <div className="flex">
+          <Link href="/creators" className="font-medium bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-400 transition-colors">
+            Creators
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/app/components/ImageSlider.js
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import React from "react";
+import { RiArrowRightSLine } from "react-icons/ri";
+import getCategories from "../getcategories";
+import Image from "next/image";
+
+
+export default async function ImageSlider({ articulo }) {
+  const categoria = await getCategories(articulo.categories);
+  return (
+    <div>
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-2 lg:gap-8">
+        <div className="w-full lg:w-1/2 h-[300px] md:min-h-[450px]">
+          <Link href="/">
+            <img
+              src="https://www.imagethread.com/koken/storage/cache/images/000/033/marker-landscape-d10,medium.2x.1475998338.jpg"
+              alt=""
+              className="rounded-lg w-full h-[300px] md:min-h-[450px] object-cover"
+            />
+          </Link>
+        </div>
+
+        <div className="lg:w-1/2 min-h-[450px] flex flex-col justify-evenly px-4 lg:pr-4">
+          <span
+            id="categoria"
+            className="font-bold text-white bg-cyan-900 w-[70px] text-center rounded-xl uppercase text-xs px-2 py-1"
+          >
+            {categoria}
+          </span>
+
+          <h2
+            id="titulo"
+            className="text-slate-900 text-2xl md:text-4xl font-semibold"
+          >
+            {articulo.title.rendered}
+          </h2>
+
+          <div className="flex items-center">
+            {/* <img
+              className="w-10 h-10 rounded-full mr-2 object-cover"
+              src="https://aishlatino.com/wp-content/uploads/2021/11/que-tipo-de-persona-te-gustaria-ser-730x411-SP.jpg"
+              alt="Avatar of Jonathan Reinink"
+            /> */}
+            <div className="text-sm">
+              <p id="autor" className="text-gray-600 leading-none">
+                by <span className="font-bold">Laura Adans</span>
+              </p>
+              <p id="date" className="text-gray-600"></p>
+            </div>
+          </div>
+
+          <p
+            id="sinopsis"
+            className="text-gray-400 text-lg font-semibold pb-4 border-b-2 border-gray-300"
+          >
+            {articulo.excerpt.rendered}
+          </p>
+
+          <Link
+            title="continue reading"
+            href="/"
+            className="font-medium bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-400 transition-colors flex gap-2 items-center max-w-[180px]"
+          >
+            Continue reading <RiArrowRightSLine className="mt-1" />{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Continue reading <RiArrowRightSLine className="mt-1" />{" "}
